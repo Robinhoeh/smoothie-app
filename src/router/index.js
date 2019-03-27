b246f8ed2238656050835784cfbdd4563fe9da86
Robin <drumsrobin@gmail.com>
2019-03-27 19:04:19 -0400

    updated router  - added routes file

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import routes from './routes'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: () => import('@/components/Index')
-    },
-    {
-      path: '/add-smoothie',
-      name: 'AddSmoothie',
-      component: () => import('@/views/create/CreateSmoothieView')
-    },
-    {
-      path: '/edit-smoothie/:smoothie_slug',
-      name: 'EditSmoothie',
-      component: () => import('@/views/edit/EditSmoothieView')
-    },
-    {
-      path: '/edit-smoothie/:smoothie_slug/favorite-recipe',
-      name: 'FavoriteRecipe',
-      component: () => ('@/views/create/FavoriteSmoothie'),
-      meta: {transitionName: 'slide'},
-    },
-    {
-      path: '/sign-in',
-      name: 'SignIn',
-      component: () => ('@/views/auth/SignInView')
-    },
-  ]
+  routes
 })
 
---
+++ src/router/routes.js
@@ -0,0 +1,28 @@
+export default [
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('@/components/Index')
+  },
+  {
+    path: '/add-smoothie',
+    name: 'AddSmoothie',
+    component: () => import('@/views/create/CreateSmoothieView')
+  },
+  {
+    path: '/edit-smoothie/:smoothie_slug',
+    name: 'EditSmoothie',
+    component: () => import('@/views/edit/EditSmoothieView')
+  },
+  {
+    path: '/edit-smoothie/:smoothie_slug/favorite-recipe',
+    name: 'FavoriteRecipe',
+    component: () => ('@/views/create/FavoriteSmoothie'),
+    meta: { transitionName: 'slide' },
+  },
+  {
+    path: '/sign-in',
+    name: 'SignIn',
+    component: () => ('@/views/auth/SignInView')
+  },
+]
